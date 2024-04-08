d069c285a21017fec94f3f6efb92b212ceaaf0d9
PaesslerJ <PaesslerJ@NB21679>
2024-04-08 14:10:50 +0200

    "hallo Ben"

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -3,5 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        Console.WriteLine("Hello Ben!")
+        Console.ReadLine()
     End Sub
 End Module
