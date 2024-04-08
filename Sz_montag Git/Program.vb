6ffd88822651df786f29d81da1c008bcee9b77c8
PaesslerJ <PaesslerJ@NB21679>
2024-04-08 11:09:53 +0200

    "console writeline" hinzugefügtFor schleife hinzugefügt

--- Sz_montag Git/Program.vb
+++ Sz_montag Git/Program.vb
@@ -3,5 +3,14 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+
+        For i = 1 To 5
+            Console.WriteLine(i)
+            If i = 5 Then
+                Console.WriteLine("ich bin jetzt bei 5!")
+            End If
+        Next
+
+
     End Sub
 End Module
