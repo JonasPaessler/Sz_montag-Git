64d85a0c41cb42edc8b60cf78f27a00d8d3fb747
PaesslerJ <PaesslerJ@NB21679>
2024-04-08 11:09:53 +0200

    For schleife hinzugefügt

--- Sz_montag Git/Program.vb
+++ Sz_montag Git/Program.vb
@@ -3,5 +3,9 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+
+        For i = 1 To 5
+            Console.WriteLine(i)
+        Next
     End Sub
 End Module
