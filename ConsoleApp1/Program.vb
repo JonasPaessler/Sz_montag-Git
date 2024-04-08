9faf8e726607720ef3b689769b909a1a2a171a35
Benjamin Regler <benjamin.regler@sz.de>
2024-04-08 14:10:09 +0200

    Begrüßung für Jonas eingebaut

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -3,5 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        Console.WriteLine("Hallo Jonas!")
+        console.readline()
     End Sub
 End Module
